106cd0e4ee85a505f5db16585e0f6650c0490dea
Brunobahy <bruno.bahy.oliveira@gmail.com>
2026-05-22 09:54:30 -0300

    migações

--- contact/admin.py
+++ contact/admin.py
@@ -7,6 +7,12 @@ class ContatcAdmin(admin.ModelAdmin):
     list_display = ('id','first_name','last_name','phone','create_date',)
     ordering = ('-id',)
     # list_filter = ('create_date',)
-    search_fields = 'id','first_name','last_name',
+    search_fields = 'id','first_name','last_name','category',
     list_per_page = 10
     list_max_show_all= 200
+    list_display_links= 'first_name',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('-id',)
--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # id (primary key)
 # Create your models here.
 # first_name (string), last_name(string),phone (string)
@@ -9,6 +10,17 @@ from django.utils import timezone
 # category (foreign key), owner (foreign key), show (boolean)
 # picture(imagem)
 
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return f'{self.name}'
+
 # blank=True -> Retira a OBRIGATORIEDADE do campo ser preenchido
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -17,6 +29,18 @@ class Contact(models.Model):
     email = models.EmailField(max_length=250,blank=True)
     create_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True,upload_to='picture/%y/%m/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
